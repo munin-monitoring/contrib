cde616b8d0e6e06310242afdc346e512b1d6d7fc
Daniel Lo Nigro <daniel@dan.cx>
2013-06-24 22:11:49 +1000

    Add PHP OPcache plugin

---
+++ plugins/php/php_opcache.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Part of Munin PHP OPcache plugin - Refer to php_opcache for installation instructions.
+ */
+
+if (function_exists('opcache_get_status')) 
+{
+	$data = opcache_get_status();
+	$output = array(
+		'mem_used.value' => $data['memory_usage']['used_memory'],
+		'mem_free.value' => $data['memory_usage']['free_memory'],
+	);
+}
+else
+{
+	// OPCache not installed :(
+	$output = array(
+		'mem_used.value' => 0,
+		'mem_free.value' => 0,
+	);
+}
+
+header('Content-Type: text/plain');
+foreach ($output as $key => $value)
+{
+        echo $key, ' ', $value, "\n";
+}
+?>
